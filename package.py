01ff4781a3aa964ed205586ec1107c4b851c86a3
Terra Jentsch <terra.jentsch@domain.hernehill.com>
2026-06-10 11:59:59 -0500

    Changes for Windows compatibility.

--- package.py
+++ package.py
@@ -22,11 +22,9 @@ requires = [
 private_build_requires = []
 
 variants = [
-    ["python-3.7"],
     ["python-3.9"],
     ["python-3.10"],
     ["python-3.11"],
-    ["python-3.12"],
 ]
 
 
@@ -34,21 +32,17 @@ def commands():
     env.REZ_IMATH_ROOT = "{root}"
     env.Imath_ROOT = "{root}"
     env.Imath_DIR = "{root}/lib64/cmake/Imath"
-    env.LD_LIBRARY_PATH.append("{root}/lib64")
+    env.PATH.append("{root}/lib64")
 
     if "python" in resolve:
         python_ver = resolve["python"].version
         if python_ver.major == 3:
-            if python_ver.minor == 7:
-                env.PYTHONPATH.append("{root}/lib/python3.7/site-packages")
-            elif python_ver.minor == 9:
+            if python_ver.minor == 9:
                 env.PYTHONPATH.append("{root}/lib/python3.9/site-packages")
             elif python_ver.minor == 10:
                 env.PYTHONPATH.append("{root}/lib/python3.10/site-packages")
             elif python_ver.minor == 11:
                 env.PYTHONPATH.append("{root}/lib/python3.11/site-packages")
-            elif python_ver.minor == 12:
-                env.PYTHONPATH.append("{root}/lib/python3.12/site-packages")
 
 
 uuid = "repository.Imath"
